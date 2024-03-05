cf6e16986d860d44b93b137034396968284fc8cb
AnnikaLindberg <annika.edit.lindberg@gmail.com>
2024-03-05 21:29:55 +0100

    Initial commit

---
+++ sanity/nextsanityportfolio/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 't7u3qpl5',
+    dataset: 'production'
+  }
+})
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'   
+
+export default function Navbar() {
+
+return (
+    <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 px-5">
+      <Link href={"/"} className='font-bold text-3xl'>
+        <span className='text-b'>Sanityblog</span>
+        </Link>
+       
+    </nav>
+)
+    
+    }
