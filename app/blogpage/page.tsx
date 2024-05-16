c4a0a00178f9ca9c781945562b1bc4e36b0e9b6f
AnnikaLindberg <annika.edit.lindberg@gmail.com>
2024-05-16 13:31:30 +0200

    bloggtitel

--- app/blogpage/page.tsx
+++ app/blogpage/page.tsx
@@ -53,7 +53,7 @@ export default async function BlogPage() {
                     className="hover:underline"
                     href={`/blog/${post.currentSlug}`}
                   >
-                    Take me to {post.title}
+                    {post.title}
                   </Link>
                 </Button>
               </CardContent>
