79b8301d28bebce590bce10bedacf78230cce600
AnnikaLindberg <annika.edit.lindberg@gmail.com>
2024-05-16 08:58:25 +0200

    fixed responsive view for annika page

--- app/components/annika/about.tsx
+++ app/components/annika/about.tsx
@@ -18,17 +18,16 @@ export default function About() {
   return (
     <motion.section
       ref={ref}
-      className="mb-28 max-w-[45rem]  leading-8 sm:mb-40 scroll-mt-28"
+      className="mb-28 max-w-[45rem] leading-8 sm:mb-40 scroll-mt-28"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
       id="about"
     >
-      {' '}
-      <h3 className="text-3xl text-center text-teal-900 dark:text-teal-50 font-bold ">
+      <h3 className="text-3xl text-center text-teal-900 dark:text-teal-50 font-bold">
         15 + års erfarenhet av tillgänglighetsarbete, projektledning och
-        utbildning{' '}
-      </h3>{' '}
+        utbildning
+      </h3>
       <p className="mt-4">
         För mig är tillgänglighet en naturlig del av webbutvecklings alla faser,
         från design till utveckling och testning. Jag praktiserar tillgänglighet
@@ -45,18 +44,19 @@ export default function About() {
         tillgängliga digitala lösningar.
       </p>
       <hr className="w-10 h-1 mx-auto my-8 bg-cyan-600 border-0 rounded md:my-12 dark:bg-pink-500" />
-      <h3 className="text-2xl text-center font-bold mb-8 mt-8  mr-5 text-cyan-800 dark:text-cyan-200">
+      <h3 className="text-2xl text-center font-bold mb-8 mt-8 mr-5 text-cyan-800 dark:text-cyan-200">
         Driven och engagerad lagspelare som älskar nya utmaningar, att
-        programmera och att dricka kaffe{' '}
-      </h3>{' '}
+        programmera och att dricka kaffe
+      </h3>
       <p className="text-lg font-medium mt-14 text-center">
         Låt oss boka en förutsättningslös fika och prata om hur vi kan
         samarbeta!
-      </p>{' '}
-      <div className="flex flex-row items-center justify-around mt-4">
+      </p>
+      <div className="flex flex-col items-center justify-around mt-4 md:flex-row">
+        {/* Adjusted padding for smaller screens */}
         <Link
           href="https://www.annikalindberg-ts.com/#contact"
-          className="group bg-gradient-to-bl from-fuchsia-900 to-pink-600  text-white px-7 py-3 flex items-center gap-2 rounded-lg outline-none focus:scale-110 hover:scale-110 hover:bg-orange-900 active:scale-105 transition dark:text-stone-100 dark:hover:bg-red-900 cursor-pointer borderBlack"
+          className="group mt-6 bg-gradient-to-bl from-fuchsia-900 to-pink-600 text-white px-5 py-2 md:px-7 md:py-3 flex items-center md:gap-2 rounded-lg outline-none focus:scale-110 hover:scale-110 hover:bg-orange-900 active:scale-105 transition dark:text-stone-100 dark:hover:bg-red-900 cursor-pointer borderBlack"
           rel="noopener noreferrer"
           target="_blank"
           aria-label="Boka möte med mig - öppnas i ny flik"
@@ -71,11 +71,11 @@ export default function About() {
           width={200}
           height={200}
           className="mb-12"
-        />{' '}
+        />
       </div>
       <div className="flex items-center justify-center">
         <motion.div
-          className="flex flex-col sm:flex-row items-center justify-center gap-6 px-4 text-lg font-medium"
+          className="flex flex-col sm:flex-row items-center justify-center gap-4 px-4 text-lg font-medium"
           initial={{ opacity: 0, y: 100 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{
@@ -83,7 +83,7 @@ export default function About() {
           }}
         >
           <a
-            className="group bg-red-50 hover:bg-red-100 px-7 py-3 flex items-center gap-2 rounded-lg outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:hover:text-white-50 dark:hover:bg-zinc-700 dark:bg-gradient-to-br from-zinc-900 to-cyan-900"
+            className="group mb-6 bg-red-50 hover:bg-red-100 px-5 py-2 md:px-7 md:py-3 flex items-center gap-2 rounded-lg outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:hover:text-white-50 dark:hover:bg-zinc-700 dark:bg-gradient-to-br from-zinc-900 to-cyan-900"
             href="/AnnikaLindberg-CV1.2.pdf"
             download="AnnikaLindberg-CV.pdf"
             aria-label="Download Annikas CV - opens in new tab"
@@ -92,31 +92,37 @@ export default function About() {
             <HiDownload className="opacity-60 group-hover:translate-y- transition" />
             <FaRegFilePdf />
           </a>
-          <div className="flex-row flex items-center justify-center gap-2 ">
-            {' '}
-            <p className="text-sm">Lägg till mig på LinkedIn</p>
-            <Link
-              className="bg-white-50 p-3 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white"
-              href="https://www.linkedin.com/in/annika-lindberg-frontend-developer/"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Visit Annikas Linkedin profile page - opens in new tab"
-              title="External link to Annikas LinkedIn"
-            >
-              {' '}
-              <BsLinkedin />
-            </Link>
-            <p className="text-sm">och GitHub</p>
-            <Link
-              className="bg-white-50 p-3 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white"
-              href="https://github.com/annikalindberg"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="External link to Annikas GitHub"
-              aria-label="Visit Annikas Github profile page - opens in new tab"
-            >
-              <FaGithubSquare />
-            </Link>
+          <div className="flex flex-col sm:flex-row items-center justify-center gap-2 md:gap-4">
+            <div className="flex flex-col items-center sm:flex-row">
+              <p className="text-sm mb-2 sm:mb-0 sm:mr-2">
+                Lägg till mig på LinkedIn
+              </p>
+              {/* Adjusted to block elements */}
+              <Link
+                className="block bg-white-50 p-2 md:p-3 text-gray-700 hover:text-gray-950 flex items-center gap-1 md:gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white"
+                href="https://www.linkedin.com/in/annika-lindberg-frontend-developer/"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Visit Annikas Linkedin profile page - opens in new tab"
+                title="External link to Annikas LinkedIn"
+              >
+                <BsLinkedin />
+              </Link>
+            </div>
+            <div className="flex flex-col items-center sm:flex-row">
+              <p className="text-sm mb-2 sm:mb-0 sm:mr-2">och GitHub</p>
+              {/* Adjusted to block elements */}
+              <Link
+                className="block bg-white-50 p-2 md:p-3 text-gray-700 flex items-center gap-1 md:gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white"
+                href="https://github.com/annikalindberg"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="External link to Annikas GitHub"
+                aria-label="Visit Annikas Github profile page - opens in new tab"
+              >
+                <FaGithubSquare />
+              </Link>
+            </div>
           </div>
         </motion.div>
       </div>
