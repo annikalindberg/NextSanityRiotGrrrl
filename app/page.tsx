04de62a9bf0d0ed56a1c1fd9044f0a3a2a3c359d
AnnikaLindberg <annika.edit.lindberg@gmail.com>
2024-05-30 15:57:34 +0200

    tog bort blogcards från page

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,20 @@ import TjansterCards from './components/homepagebanners/TjansterCards'
 import OmOss from './components/om-oss/OmOss'
 import Tillganglighet from './components/Tillganglighet'
 import ErbjudandeCards from './components/homepagebanners/ErbjudandenCards'
-import BlogCards from './components/BlogCards'
-
+/* import BlogCards from './components/BlogCards'
+ */
 export default function Home() {
   return (
     <>
       <Hero />
       <ErbjudandeCards />
-
       <TjansterCards />
       <OmOss />
       <div className="bg-gradient-to-r from-blue-200 to-cyan-200 dark:from-blue-950 dark:to-cyan.950 pt-5 md:pt-10 lg:pt-20 min-h-screen">
         <Tillganglighet />
       </div>
-      <BlogCards />
+      {/*       <BlogCards />
+       */}{' '}
     </>
   )
 }
