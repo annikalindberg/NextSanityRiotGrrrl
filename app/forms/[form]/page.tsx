cc632a56f9bb2ac4dd3428db238e617eb4949418
AnnikaLindberg <annika.edit.lindberg@gmail.com>
2024-06-28 13:17:36 +0200

    deployement issues in forms-pafe

--- app/forms/[form]/page.tsx
+++ app/forms/[form]/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import React from 'react'
 import toast from 'react-hot-toast'
 import { auditRequest } from '@/actions/auditRequest'
@@ -9,22 +10,14 @@ export default function DynamicForm({ params }: { params: { form: string } }) {
 
   const auditType = decodeURIComponent(params.form).replace(/-/g, ' ')
   return (
-    <section className="max-w-3xl mx-auto px-4 ">
+    <section className="max-w-3xl mx-auto px-4">
       <motion.section
         id="contact"
-        className="mb-20 sm:mb-28 w-[min(100%,38rem)] "
-        initial={{
-          opacity: 0,
-        }}
-        whileInView={{
-          opacity: 1,
-        }}
-        transition={{
-          duration: 1,
-        }}
-        viewport={{
-          once: true,
-        }}
+        className="mb-20 sm:mb-28 w-[min(100%,38rem)]"
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        viewport={{ once: true }}
       >
         <h1 className="mt-20 text-4xl dark:text-white">
           Välkommen till bokningsförfrågan för: &quot;{formTitle}!&quot;
@@ -37,38 +30,20 @@ export default function DynamicForm({ params }: { params: { form: string } }) {
           För att ta del av vårat sommar-erbjudande där vi ger 50% rabatt till
           de första som bokar vårt paket &quot;Kickstart - snabbt och
           enkelt&quot; - skriv in rabattkoden &quot;semester&quot; i
-          meddelandefältet.{' '}
+          meddelandefältet.
         </p>
         <p className="text-gray-700 mt-6 dark:text-white/80">
-          {' '}
           Observera att detta är ett begränsat erbjudande och gäller bara de som
           är bland de första 4 att boka. Du får då en offert med 60% rabatt av
-          ordinarie pris.{' '}
+          ordinarie pris.
         </p>
         <p>
           Blir du inte en av de första 4 så kommer du att få en offert med
-          ordinarie pris efter att du skickat in din förfrågan.{' '}
+          ordinarie pris efter att du skickat in din förfrågan.
         </p>
         <p className="text-gray-700 mt-6 dark:text-white/80">
           Vi återkommer alltid till dig inom 24 timmar.
         </p>
-        {/*        <h2>Kostnadsfri tillgänglighets-audit</h2>
-        <p className="text-gray-700 mt-6 dark:text-white/80">
-          Gäller ditt ärende en bokning av &quot;kostnadsfri audit&quot; kommer
-          vi att skicka en sammanställlning med resultat och förslag på åtgärder
-          inom 1-5 arbetsdagar, till den e-post adress du uppger. Vid oväntat
-          högt tryck kan det ta längre tid.{' '}
-        </p> */}
-        {/*         <p className="text-gray-700 mt-6 dark:text-white/80">
-          Please contact me directly at{' '}
-          <a
-            className="underline"
-            href="mailto:annika.edit.lindberg&#64;gmail&#46;com"
-          >
-            annika.edit.lindberg&#64;gmail&#46;com
-          </a>{' '}
-          or through this form.
-        </p> */}
         <form
           className="mt-10 flex flex-col dark:text-black"
           onSubmit={async event => {
@@ -86,8 +61,6 @@ export default function DynamicForm({ params }: { params: { form: string } }) {
             toast.success('Förfrågan skickad!')
           }}
         >
-          {/* Hidden input to include the audit type */}
-
           <input type="hidden" name="auditType" value={auditType} />
           <label
             htmlFor="senderEmail"
@@ -131,10 +104,9 @@ export default function DynamicForm({ params }: { params: { form: string } }) {
             maxLength={500}
             placeholder="https://www.dinwebbplats.se"
           />
-
           <button
             type="submit"
-            className="bg-gradient-to-r from-green-200 via-red-200 to-purple-200 text-slate-950 font-bold py-3 rounded-lg mt-5 focus:  "
+            className="bg-gradient-to-r from-green-200 via-red-200 to-purple-200 text-slate-950 font-bold py-3 rounded-lg mt-5"
           >
             Skicka förfrågan
           </button>
